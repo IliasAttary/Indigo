9e7bffad65a59601ffdaad2614e4a47553fd6fad
Dustin Parchatka <dustin.parchatka@tu-dortmund.de>
2023-12-12 14:43:05 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,9 +32,9 @@ application {
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
-    implementation(group = "tools.aqua", name = "bgw-gui", version = "0.8.1")
-    implementation(group = "tools.aqua", name = "bgw-net-common", version = "0.8.1")
-    implementation(group = "tools.aqua", name = "bgw-net-client", version = "0.8.1")
+    implementation(group = "tools.aqua", name = "bgw-gui", version = "0.9")
+    implementation(group = "tools.aqua", name = "bgw-net-common", version = "0.9")
+    implementation(group = "tools.aqua", name = "bgw-net-client", version = "0.9")
     implementation(group = "edu.udo.cs.sopra", name = "ntf", version = "1.0")
 }
 
