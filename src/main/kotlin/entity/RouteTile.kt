f95e5fa8f36a57e11eb23383726ff3cbff074329
Eser Kayali <eser.kayali@tu-dortmund.de>
2023-12-15 18:36:35 +0100

    Changed the type of heldTile to RouteTile in Player. Implemented checkPlacement() and added an if in rotateTile(). Made RouteTile Serializable.

--- src/main/kotlin/entity/RouteTile.kt
+++ src/main/kotlin/entity/RouteTile.kt
@@ -1,10 +1,12 @@
 package entity
+import kotlinx.serialization.Serializable
 
 /**
  * Entity that inherits from Tile to represent a Route Tile
  * @property gemPositions map that has the positions of the gems on the Route Tile
  * @property tileType the type of the tile with its paths
  */
-data class RouteTile(val tileType: TileType?) : Tile() {
+@Serializable
+data class RouteTile(val tileType: TileType) : Tile() {
     val gemPositions: MutableMap<Int, Gem> = mutableMapOf()
 }
