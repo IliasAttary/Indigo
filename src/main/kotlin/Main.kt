abcb2f38c72bcc35b12308cf90842c97ad712546
tanja <tanja.hornig@tu-dortmund.de>
2023-12-16 17:26:28 +0100

    added show indigo application

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
+import view.IndigoApplication
+
 /**
  * main function that starts the [IndigoApplication]
  */
 fun main() {
+    IndigoApplication().show()
     println("Application ended. Goodbye")
 }
