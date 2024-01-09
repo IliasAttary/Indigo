0663bca8d58e9fc810016c9e238420641e91c78c
tanja <tanja.hornig@tu-dortmund.de>
2024-01-09 18:09:29 +0100

    RulesScene

--- src/main/kotlin/view/RulesScene.kt
+++ src/main/kotlin/view/RulesScene.kt
@@ -1,8 +1,7 @@
 package view
 
-import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.core.MenuScene
-import tools.aqua.bgw.visual.ImageVisual
+
 
 class RulesScene : MenuScene(500, 500), Refreshable {
 
