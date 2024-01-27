4bad963ab901cb752c3844abc7f3ec2b9c25d2e7
Linus <>
2024-01-27 19:39:01 +0100

    add HowToPlay to distribution.zip

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,10 @@ dependencies {
 
 tasks.distZip {
     archiveFileName.set("distribution.zip")
+    into("") {
+        from(".")
+        include("HowToPlay.pdf")
+    }
     destinationDirectory.set(layout.projectDirectory.dir("public"))
 }
 
